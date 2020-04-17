1b0468dd89c679f910c689808bd323808a297bca
ashrakrahman <lipu@mpower-social.com>
2020-04-17 22:56:45 +0600

    Add home function to the account views file

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {
+    })
