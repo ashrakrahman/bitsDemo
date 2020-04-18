16cc51f1166c15baec0c298a03949693b6bd2793
ashrakrahman <lipu@mpower-social.com>
2020-04-18 16:30:28 +0600

    Add create web hook url

--- githubTask/urls.py
+++ githubTask/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^github/', views.github_task_view, name='github_task_view'),
+    url(r'^webwhook/(?P<repo_name>[^/]+)/(?P<user>[^/]+)/',
+        views.github_webhook_view, name='github_webhook_view'),
     url(r'^process/callback/', views.process_callback_view,
         name='process_callback_view'),
     url(r'^getGithubRepoListByUser/', views.getGithubRepoListByUser,
